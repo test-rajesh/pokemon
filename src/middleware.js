e4c0ab613ecb8a4cb31406116a71b8e9eb9d3856
RAJESH KHAJURIA <162429527+Rajesh892@users.noreply.github.com>
2024-08-05 16:42:50 +0530

    added middlewares

---
+++ src/middleware.js
@@ -0,0 +1,39 @@
+// middleware.js
+
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  const response = NextResponse.next();
+
+  // Set security headers
+  response.headers.set("Content-Security-Policy", "default-src 'self'");
+  response.headers.set("X-DNS-Prefetch-Control", "off");
+  response.headers.set("X-Frame-Options", "SAMEORIGIN");
+  response.headers.set(
+    "Strict-Transport-Security",
+    "max-age=15552000; includeSubDomains"
+  );
+  response.headers.set("X-Download-Options", "noopen");
+  response.headers.set("X-Content-Type-Options", "nosniff");
+  response.headers.set("X-Permitted-Cross-Domain-Policies", "none");
+  response.headers.set("Referrer-Policy", "no-referrer");
+
+  // Additional headers
+  response.headers.set("Cross-Origin-Resource-Policy", "same-origin");
+  response.headers.set("Cross-Origin-Embedder-Policy", "require-corp");
+  response.headers.set(
+    "Permissions-Policy",
+    "geolocation=(self), microphone=()"
+  );
+  response.headers.set("Expect-CT", "max-age=86400, enforce");
+  response.headers.set(
+    "Feature-Policy",
+    "geolocation 'self'; microphone 'none'"
+  );
+
+  return response;
+}
+
+export const config = {
+  matcher: "/:path*",
+};
